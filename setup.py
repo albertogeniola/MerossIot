77dbc02123ab6f1aab4104b2b43e75cb94719856
Alberto Geniola <albertogeniola@gmail.com>
2018-11-24 23:52:20 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Meross MSS310',
+    version='0.1.0',
+    packages=['.', 'supported_devices'],
+    url='https://github.com/albertogeniola/MerossIot',
+    license='MIT',
+    author='Alberto Geniola',
+    author_email='albertogeniola@gmail.com',
+    description='A simple library to deal with Meross MSS310 smart plug',
+    classifiers=[
+              'Development Status :: 4 - Beta',
+              'Intended Audience :: Developers',
+              'Programming Language :: Python :: 3'
+          ]
+)
