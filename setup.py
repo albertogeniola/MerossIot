0f4ff4ef5e872ed8e8a8a67bd93b89fef6f2d85e
Alberto Geniola <albertogeniola@gmail.com>
2018-11-25 00:04:20 +0100

    Changed name of pip package to meross-iot

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='Meross MSS310',
+    name='meross-iot',
     version='0.1.0',
     packages=['.', 'supported_devices'],
     url='https://github.com/albertogeniola/MerossIot',
