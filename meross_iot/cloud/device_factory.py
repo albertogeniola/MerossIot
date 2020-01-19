af6e079bd7bc8f2ce3ba721efbe39b3f509d3824
Thomas Dippon <43482375+tdippon@users.noreply.github.com>
2020-01-19 10:37:10 +0100

    added support for SensorSubDevice

--- meross_iot/cloud/device_factory.py
+++ meross_iot/cloud/device_factory.py
@@ -32,5 +32,7 @@ def build_subdevice_wrapper(cloud_client,
                             ):
     if device_type.startswith('mts'):
         return ValveSubDevice(cloud_client, subdevice_id=device_id, parent_hub=parent_hub, **device_specs)
+    elif device_type.startswith('ms100'):
+        return SensorSubDevice(cloud_client, subdevice_id=device_id, parent_hub=parent_hub, **device_specs)
     else:
         return GenericSubDevice(cloud_client, subdevice_id=device_id, parent_hub=parent_hub, **device_specs)
