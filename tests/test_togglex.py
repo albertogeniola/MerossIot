8d124ab83c6b1e0ed6e378f520fe3f9652c2b6d7
Alberto Geniola <albertogeniola@gmail.com>
2020-05-08 16:43:56 +0200

    Added push notification test for togglex

--- tests/test_togglex.py
+++ tests/test_togglex.py
@@ -49,5 +49,44 @@ class TestToggleX(AioHTTPTestCase):
         r = await self.test_device.turn_off()
         self.assertFalse(self.test_device.is_on)
 
+    async def test_toggle_push_notification(self):
+        if self.test_device is None:
+            self.skipTest("No ToggleX device has been found to run this test on.")
+            return
+
+        # Create a new manager
+        new_meross_client = await MerossHttpClient.async_from_user_password(email=EMAIL, password=PASSWORD)
+        m = None
+        try:
+            # Retrieve the same device with another manager
+            m = MerossManager(http_client=new_meross_client)
+            await m.async_device_discovery()
+            devs = m.find_device(uuids=(self.test_device.uuid))
+            dev = devs[0]
+
+            # Turn off device to start from a clean state
+            r = await self.test_device.turn_off()
+            await asyncio.sleep(2)
+
+            # Turn on the device
+            r = await self.test_device.turn_on()
+            # Wait a bit and make sure the other manager received the push notification
+            await asyncio.sleep(2)
+            self.assertTrue(self.test_device.is_on)
+            self.assertTrue(dev.is_on)
+
+            # Turn off the device
+            await asyncio.sleep(1)
+            r = await self.test_device.turn_off()
+            # Wait a bit and make sure the other manager received the push notification
+            await asyncio.sleep(2)
+            self.assertFalse(self.test_device.is_on)
+            self.assertFalse(dev.is_on)
+
+        finally:
+            if m is not None:
+                m.close()
+            await new_meross_client.async_logout()
+
     async def tearDownAsync(self):
         await self.meross_client.async_logout()
