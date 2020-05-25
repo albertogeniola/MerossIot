ebd548b867a9f1fad6e256bb1d723db63ccbd0dd
Alberto Geniola <albertogeniola@gmail.com>
2020-05-25 09:48:41 +0200

    Renamed static and templates directories

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ author = 'Alberto Geniola'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -48,4 +48,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
