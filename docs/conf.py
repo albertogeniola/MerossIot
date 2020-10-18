06a49e5354950a16bfb0e4e05987419d74f450a3
Alberto Geniola <alberto_geniola@carrefour.com>
2020-10-18 13:25:56 +0200

    changed html_static_path

--- docs/conf.py
+++ docs/conf.py
@@ -67,8 +67,8 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_images']
-html_extra_path = ['_custom', 'static']
+html_static_path = ['']
+html_extra_path = ['_custom']
 
 
 # This function reads in a variable and inserts it into JavaScript
