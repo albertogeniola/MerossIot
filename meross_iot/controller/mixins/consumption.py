d1420509769e000cdcae2c28d331c18983f88822
Alberto Geniola <alberto_geniola@carrefour.com>
2020-11-21 16:04:27 +0100

    - Added Consumption Mixin

--- meross_iot/controller/mixins/consumption.py
+++ meross_iot/controller/mixins/consumption.py
@@ -37,3 +37,32 @@ class ConsumptionXMixin(object):
         } for x in data]
 
         return res
+
+
+class ConsumptionMixin(object):
+    _execute_command: callable
+
+    def __init__(self, device_uuid: str,
+                 manager,
+                 **kwargs):
+        super().__init__(device_uuid=device_uuid, manager=manager, **kwargs)
+
+    async def async_get_daily_power_consumption(self, channel=0, *args, **kwargs) -> List[dict]:
+        """
+        Returns the power consumption registered by this device.
+
+        :param channel: channel to read data from
+
+        :return: the historical consumption data
+        """
+        # TODO: returning a nice PowerConsumtpionReport object rather than a list of dict?
+        result = await self._execute_command("GET", Namespace.CONTROL_CONSUMPTION, {'channel': channel})
+        data = result.get('consumption')
+
+        # Parse the json data into nice-python native objects
+        res = [{
+                'date': datetime.strptime(x.get('date'), _DATE_FORMAT),
+                'total_consumption_kwh': float(x.get('value'))/1000
+        } for x in data]
+
+        return res
