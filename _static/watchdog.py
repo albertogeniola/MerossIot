61f6f09e6fd41842cef38f7c957f40b54b69f777
GitHub Action <action@github.com>
2020-11-25 20:32:39 +0000

    Update documentation

--- _static/watchdog.py
+++ _static/watchdog.py
@@ -1,5 +1,6 @@
-from livereload import Server, shell
 import asyncio
+
+from livereload import Server, shell
 
 # The first line is needed for Windows and Python 3.8
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
