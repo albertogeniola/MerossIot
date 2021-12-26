f7a42ed1b57d31a7debf2ba388572ed045e7bdd3
Alberto Geniola <albertogeniola@gmail.com>
2021-12-26 12:38:06 +0100

    Commented all known devices except for subdevices.

--- meross_iot/controller/known/bulbs.py
+++ meross_iot/controller/known/bulbs.py
@@ -4,35 +4,35 @@ from meross_iot.controller.mixins.system import SystemAllMixin, SystemOnlineMixi
 from meross_iot.controller.mixins.toggle import ToggleXMixin
 from meross_iot.model.enums import Namespace
 
-
-class MSL120(SystemAllMixin, SystemOnlineMixin, LightMixin, ToggleXMixin, BaseDevice):
-    """
-    MSL120 Smart Bulb
-    """
-    def __init__(self, device_uuid: str, manager, **kwargs):
-        if 'channels' not in kwargs:
-            kwargs['channels'] = [{}]
-
-        # MSL120 has the following capacity
-        custom_abilities = {Namespace.CONTROL_LIGHT.value: {'capacity': 7}}
-        if hasattr(self, "_abilities_spec"):
-            self._abilities_spec.update(custom_abilities)
-        else:
-            self._abilities_spec = custom_abilities
-
-        super().__init__(device_uuid=device_uuid,
-                         manager=manager,
-                         **kwargs)
-
-
-class MSL100(SystemAllMixin, SystemOnlineMixin, LightMixin, ToggleXMixin, BaseDevice):
-    """
-    MSL100 Smart Bulb
-    """
-    def __init__(self, device_uuid: str, manager, **kwargs):
-        if 'channels' not in kwargs:
-            kwargs['channels'] = [{}]
-        super().__init__(device_uuid=device_uuid,
-                         manager=manager,
-                         **kwargs)
-
+#
+# class MSL120(SystemAllMixin, SystemOnlineMixin, LightMixin, ToggleXMixin, BaseDevice):
+#     """
+#     MSL120 Smart Bulb
+#     """
+#     def __init__(self, device_uuid: str, manager, **kwargs):
+#         if 'channels' not in kwargs:
+#             kwargs['channels'] = [{}]
+#
+#         # MSL120 has the following capacity
+#         custom_abilities = {Namespace.CONTROL_LIGHT.value: {'capacity': 7}}
+#         if hasattr(self, "_abilities_spec"):
+#             self._abilities_spec.update(custom_abilities)
+#         else:
+#             self._abilities_spec = custom_abilities
+#
+#         super().__init__(device_uuid=device_uuid,
+#                          manager=manager,
+#                          **kwargs)
+#
+#
+# class MSL100(SystemAllMixin, SystemOnlineMixin, LightMixin, ToggleXMixin, BaseDevice):
+#     """
+#     MSL100 Smart Bulb
+#     """
+#     def __init__(self, device_uuid: str, manager, **kwargs):
+#         if 'channels' not in kwargs:
+#             kwargs['channels'] = [{}]
+#         super().__init__(device_uuid=device_uuid,
+#                          manager=manager,
+#                          **kwargs)
+#
