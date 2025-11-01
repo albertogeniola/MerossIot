8ef29a6fc4f23ffadd1cf79348fcd1de48f4cb4b
Alberto Geniola <albertogeniola@gmail.com>
2025-11-01 15:19:00 +0100

    Refactor AlarmMixin

--- meross_iot/controller/mixins/alarm.py
+++ meross_iot/controller/mixins/alarm.py
@@ -1,15 +1,16 @@
 import logging
-from typing import Optional, List, Any
 from collections import deque
+from typing import Any
+
+from meross_iot.controller.device import BaseDevice
 from meross_iot.model.enums import Namespace
-from meross_iot.model.push.generic import GenericPushNotification
 
 _LOGGER = logging.getLogger(__name__)
 
 _MAX_ALARM_EVENTS_MEMORY = 10
 
-class AlarmMixin(object):
-    _execute_command: callable
+
+class AlarmMixin(BaseDevice):
     __last_alarm_events: deque
 
     def __init__(self, device_uuid: str,
@@ -18,10 +19,10 @@ class AlarmMixin(object):
         super().__init__(device_uuid=device_uuid, manager=manager, **kwargs)
         self.__last_alarm_events = deque(maxlen=_MAX_ALARM_EVENTS_MEMORY)
 
-    # Note: we are not hooking async_handle_update, as it seems the SYSTEM_ALL update
+    # Note: we are not overriding async_handle_update, as it seems the SYSTEM_ALL update
     #  does not carry information about latest alarms.
 
-    async def _async_handle_push_notification(self, namespace:str, data:Any) -> bool:
+    async def _async_handle_push_notification(self, namespace: Namespace, data: Any) -> bool:
         locally_handled = False
         if namespace == Namespace.CONTROL_ALARM:
             # Note: we are not storing the channel the alarm refers to.
