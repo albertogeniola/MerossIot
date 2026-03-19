921276be7bc4f6e8e34ad6f4cdff8b68ee3309af
OlivierDech <lolisteph@gmail.com>
2026-03-19 14:08:17 +0100

    Update subdevice.py with new class Ms200Sensor

--- meross_iot/controller/subdevice.py
+++ meross_iot/controller/subdevice.py
@@ -598,3 +598,142 @@ class Ms405Sensor(GenericSubDevice):
 
     def __repr__(self) -> str:
         return f"<Ms400Device(uuid={self.uuid}, is_leaking={self.is_leaking})>"
+
+class Ms200Sensor(GenericSubDevice):
+    """
+    Class that represents a Meross MS200 Door Window Sensor.
+    """
+
+    def __init__(self, hubdevice_uuid: str, subdevice_id: str, manager, max_events_queue_len=30, **kwargs):
+        super().__init__(hubdevice_uuid, subdevice_id, manager, **kwargs)
+
+        self._last_active_time: Optional[int] = None
+        # Represents the last time we contacted the device
+
+        self.__state: Optional[bool] = None
+        # Represents the current state
+
+        self.__last_event_ts: Optional[int] = None
+        # Represents the timestamp of the last sample (current state sampling)
+
+        self.__cached_events: deque = deque(maxlen=max_events_queue_len)
+        # Last N samples we collected
+
+        self.__last_opened_event_ts: Optional[int] = None
+        # Last timestamp we've seen an opening
+
+    @property
+    def is_opened(self) -> Optional[bool]:
+        """
+        Returns the latest updated state available , if available.
+        """
+        return self.__state
+
+    @property
+    def latest_sample_time(self) -> Optional[int]:
+        """
+        Returns the timestamp (GMT) of the latest available sampling.
+        """
+        return self.__last_event_ts
+
+    @property
+    def latest_detected_opening_ts(self) -> Optional[int]:
+        """
+        Return the timestamp (GMT) of the latest time the sensor sampled an opening
+        """
+        return self.__last_opened_event_ts
+
+    @property
+    def get_last_events(self) -> List[Dict]:
+        """
+        Returns the last cached items
+        """
+        return [x for x in self.__cached_events]
+
+    async def async_update(self,
+                           timeout: Optional[float] = None,
+                           *args,
+                           **kwargs) -> None:
+        # Make sure we issue an update at HUB level first
+        await super().async_update()
+
+        # We also need to trigger an update request for this specific sub-device
+        result = await self._hub._execute_command(method="GET",
+                                                  namespace=Namespace.HUB_SENSOR_ALL,
+                                                  payload={'all': [{'id': self.subdevice_id}]},
+                                                  timeout=timeout)
+
+        # Retrieve the sub-device specific data and update the status
+        subdevices_states = result.get('all')
+        for subdev_state in subdevices_states:
+            subdev_id = subdev_state.get('id')
+            if subdev_id != self.subdevice_id:
+                continue
+            await self.async_handle_subdevice_notification(namespace=Namespace.HUB_SENSOR_ALL, data=subdev_state)
+            break
+
+    def _handle_opening_fresh_data(self, open: bool, timestamp: int):
+        # If handling an event with an older timestamp than the one we have, just discard it.
+        if self.latest_sample_time is not None and timestamp <= self.latest_sample_time:
+            return
+
+        # If this is the first update or if it's more recent than the last we have, update the current state.
+        if self.__last_event_ts is None or timestamp >= self.__last_event_ts:
+            self.__last_event_ts = timestamp
+            self.__state = open
+
+        # If the event is a leak and is more recent than the latest leak event, update it.
+        if open and (self.__last_opened_event_ts is None or timestamp >= self.__last_opened_event_ts):
+            self.__last_opened_event_ts = timestamp
+
+        # In any case, register the event in the queue
+        self.__cached_events.append({
+            "status": open,
+            "lmTime": timestamp
+        })
+
+    async def async_handle_push_notification(self, namespace: Namespace, data: dict) -> bool:
+        locally_handled = False
+        if namespace == Namespace.HUB_ONLINE:
+            update_element = self._prepare_push_notification_data(data=data, filter_accessor='online')
+            if update_element is not None:
+                self._online = OnlineStatus(update_element.get('status', -1))
+                locally_handled = True
+        elif namespace == Namespace.HUB_SENSOR_DOORWINDOW:
+            opening = data.get('doorWindow')
+            latestOpening = opening.get('status')
+            latestOpeningTime = opening.get('lmTime')
+            self._handle_opening_fresh_data(open=latestOpening==1, timestamp=latestOpeningTime)
+            locally_handled = True
+
+        return locally_handled
+
+    async def async_handle_subdevice_notification(self, namespace: Namespace, data: dict) -> bool:
+        locally_handled = False
+        if namespace == Namespace.HUB_ONLINE:
+            self._online = OnlineStatus(data.get('online', {}).get('status', -1))
+            self._last_active_time = data.get('online', {}).get('lastActiveTime')
+        elif namespace == Namespace.HUB_SENSOR_DOORWINDOW:
+            latestOpening = data.get('status')
+            latestOpeningTime = data.get('lmTime')
+            self._handle_opening_fresh_data(open=latestOpening==1, timestamp=latestOpeningTime)
+            locally_handled = True
+        elif namespace == Namespace.HUB_SENSOR_ALL:
+            self._online = OnlineStatus(data.get('online', {}).get('status', -1))
+            opening = data.get('doorWindow')
+            if opening is not None:
+                latestOpening = opening.get('status')
+                latestOpeningTime = opening.get('lmTime')
+                self._handle_opening_fresh_data(open=latestOpening == 1, timestamp=latestOpeningTime)
+
+            locally_handled = True
+        else:
+            _LOGGER.warning(f"Could not handle event %s in subdevice %s handler", namespace, self.name)
+
+        # Always call the parent handler when done with local specific logic. This gives the opportunity to all
+        # ancestors to catch all events.
+        parent_handled = await super().async_handle_push_notification(namespace=namespace, data=data)
+        return locally_handled or parent_handled
+
+    def __repr__(self) -> str:
+        return f"<Ms200Device(uuid={self.uuid}, is_opened={self.is_opened})>"
